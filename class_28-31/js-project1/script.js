5cad5b7d27f7b1b846d8ffc35001ca134b9384f3
Prasanth S <118365908+PRASANTH201120@users.noreply.github.com>
2025-08-01 10:41:52 +0530

    js-1

---
+++ class_28-31/js-project1/script.js
@@ -0,0 +1,20 @@
+// function red() {
+//   document.getElementById("img").style.backgroundColor = "#f18973";
+// }
+function red() {
+  document.getElementById("img-color").style.backgroundColor = "#e87058ff";
+}
+function blue() {
+  document.getElementById("img-color").style.backgroundColor = "#034f84";
+}
+function green() {
+  document.getElementById("img-color").style.backgroundColor = "#86af49";
+}
+function image() {
+  document.getElementById("img").src =
+    "./images/pngimg.com - mustang_PNG40656.png";
+}
+function image1() {
+  document.getElementById("img").src =
+    "./images/pngimg.com - ford_PNG102954.png";
+}
